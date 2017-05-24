346839f6d0545d7229614512b0307ac9480a171e
wangan <anntony.wang@live.com>
2017-05-24 14:32:29 +0800

    Add project files.

---
+++ LogHelper.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Security;
+
+[assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+namespace Toolkits.Log {
+    public static class LogHelper {
+
+        static LogHelper() {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
+        }
+
+        public static ILog Instance {
+            get {
+                return LogManager.GetLogger("Logger");
+            }
+        }
+    }
+}
